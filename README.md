6ea27a55b174713296c25ddee58fd11511f904dd
Burak Kazdal <burakkazdal19@gmail.com>
2022-10-27 06:08:21 +0300

    uploading files

---
+++ README.md
@@ -0,0 +1,12 @@
+# GitHub Finder
+
+This application allows us to find profiles of users on github with github api. It also allows us to see the followers of the user we found, the ones they follow, and their latest repo.
+This application allows us to find profiles of users on github with github api. It also allows us to see the followers, following and their latest repos of the user we found.
+
+For GitHub token click [here](https://github.com/settings/tokens)
+
+### Includings
+
+VanillaJS
+Bootswatch - Litera
+Fetch Api
---
+++ app.js
@@ -0,0 +1,32 @@
+// Init Github & UI
+const github = new GitHub;
+const ui = new UI;
+
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+// Search Input Event Listener
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+
+    if(userText !== '') {
+        // Make http call
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                // Show alert
+                ui.showAlert('User not found', 'alert alert-danger');
+            } else {
+                // Show profile
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+
+        })
+
+    } else {
+        // Clear profile 
+        ui.clearProfile();
+    }
+})
